9cb2d70e8147eab47bb3648222d44da78a5fdd51
Enzo Costa <enzo_costa7@hotmail.com>
2022-04-14 16:42:13 -0300

    Create layout responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import styles from './App.module.css'
 import poweredimage from './assets/powered.png'
+import leftArrowimage from './assets/leftarrow.png'
 import { GridItem } from './components/GridItem'
 
-import {levels, calculateImc} from './helpers/imc'
+import {levels, calculateImc, Level} from './helpers/imc'
 
 const App = () => {
   const [heightField, setHeightField] = useState<number>(0)
   const [weightField, setWeightField] = useState<number>(0)
+  const [toShow, setToShow] = useState<Level | null>(null)
 
   const handleCalculateButton = () => {
     if(heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField))
     } else {
       alert('Digite todos os campos.')
     }
+  }
+
+  const handleBackButton = () => {
+    setToShow(null)
+    setHeightField(0)
+    setWeightField(0)
   }
 
   return (
@@ -22,6 +31,7 @@ const App = () => {
         <div className={styles.headerContainer}>
           <img src={poweredimage} width={150} alt="" />
         </div>
+      </header>
         <div className={styles.container}>
           <div className={styles.leftSide}>
             <h1>Calcule o seu IMC.</h1>
@@ -32,25 +42,36 @@ const App = () => {
               placeholder='Digite a sua altura. Ex: 1.5 (em metros)' 
               value={heightField > 0 ? heightField : ''}
               onChange={e => setHeightField(parseFloat(e.target.value))}
+              disabled={toShow ? true : false}
             />
              <input
               type="number"
               placeholder='Digite o seu peso. Ex: 75.3 (em kg)' 
               value={weightField > 0 ? weightField : ''}
               onChange={e => setWeightField(parseFloat(e.target.value))}
+              disabled={toShow ? true : false}
             />
 
-            <button onClick={handleCalculateButton}>Calcular</button>
+            <button onClick={handleCalculateButton} disabled={toShow ? true : false} >Calcular</button>
           </div>
           <div className={styles.rightSide}>
-            <div className={styles.grid}>
-              {levels.map((item, key) => (
-                <GridItem key={key} item={item} />
-              ))}
-            </div>
+            {!toShow &&
+              <div className={styles.grid}>
+                {levels.map((item, key) => (
+                  <GridItem key={key} item={item} />
+                ))}
+              </div>
+            }
+            {toShow && 
+              <div className={styles.rightBig}>
+                <div className={styles.rightArrow} onClick={handleBackButton}>
+                  <img src={leftArrowimage} alt="" width={25} />
+                </div>
+                <GridItem item={toShow} />
+              </div>
+            }
           </div>
         </div>
-      </header>
     </div>
   )
 }
--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -16,6 +16,11 @@ export const GridItem = ({ item }: GridItemProps) => {
             <div className={styles.gridTitle}>
                 {item.title}
             </div>
+
+            {item.yourImc &&
+                <div className={styles.yourImc}>Seu IMC é de {item.yourImc} kg/m²</div>
+            }
+
             <div className={styles.gridInfo}>
                 <>
                     IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
